d5486aa0d0b82fddbb1dbb645ddcb7a5ddf6c5cd
William Halim <51820876+wiriim@users.noreply.github.com>
2025-09-13 22:23:05 +0700

    Add montserrat font

--- webpack.common.js
+++ webpack.common.js
@@ -25,6 +25,10 @@ module.exports = {
         test: /\.html$/i,
         loader: "html-loader",
       },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
 };
